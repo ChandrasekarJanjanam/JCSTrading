e4d9eabbd5fc88e58e0f0f1e6f3ed3cbbc9048e6
Chandrasekar Janjanam <chandru207@gmail.com>
2024-09-11 00:38:34 +1000

    new project
    Signed-off-by: Chandrasekar Janjanam <chandru207@gmail.com>

---
+++ src/main/java/com/trading/platform/JcsTradingApplication.java
@@ -0,0 +1,13 @@
+package com.trading.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcsTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcsTradingApplication.class, args);
+	}
+
+}
